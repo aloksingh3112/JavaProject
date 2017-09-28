9c3950ed084a9a09c45ba547402c85114089f258
aloksingh3112 <aloksingh3112@gmail.com>
2017-09-29 02:03:46 +0530

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,37 @@
+class Check{
+	
+	
+	
+	static public void Case(String st) {
+		
+		System.out.println("hello ");
+	}
+	
+}
+
+
+
+class Checks{
+
+		
+
+	static public void Case() {
+		System.out.println("hi im checks 2");
+		
+	}
+}
+
+
+
+
+public class Test {
+	
+
+	public static void main(String[] args) {
+		Check ch = new Check();
+
+//Check.Case();
+
+	}
+
+}
